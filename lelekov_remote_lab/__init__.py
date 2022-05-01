e74218b19e8ae347ecf5b13698a970981b2e346b
Kirill Mil <kirill.mil.0310@mail.ru>
2022-05-01 14:02:28 +0700

    fix

--- lelekov_remote_lab/__init__.py
+++ lelekov_remote_lab/__init__.py
@@ -1,2 +1,4 @@
 import lelekov_remote_lab.plotter
 import lelekov_remote_lab.communicator
+
+__version__ = '1.1'
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
-
+import lelekov_remote_lab
 
 setup(
     name='lelekov_remote_lab',
     description='Библиотека для управления системой ориентации',
-    version='1.0',
+    version=lelekov_remote_lab.__version__,
     packages=find_packages(),
     long_description_content_type='text/markdown',
     long_description=open(join(dirname(__file__), 'ReadMe.md'), encoding='utf-8').read(),
